47008ce75cb22c9d1c9064c91dc6dad0845806ad
Vishwanath <vishwanathmalakar@gmail.com>
2018-08-23 00:49:02 +0530

    initial files

---
+++ Perl/hello.pl
@@ -0,0 +1,6 @@
+# Strict and warnings are recommended.
+use strict;
+use warnings;
+ 
+# Print a message.
+print "Hello, World!\n";
---
+++ VBS/hello.vbs
@@ -0,0 +1,7 @@
+WScript.Echo("Hello, World!")
+Dim a,b,c,d
+a=20
+b=30
+c=a+b
+MSGBOX (c)
+MSGBOX ("ok")
