4541f399c177c915751e00beb17cc24775023c44
vishwanath malakar <vishwanathmalakar@gmail.com>
2018-12-14 19:09:24 +0530

    adding files

--- JAVA/Day9-collections/ArrayListDemo.java
+++ JAVA/Day9-collections/ArrayListDemo.java
@@ -2,11 +2,12 @@ import java.awt.List;
 import java.util.ArrayList;
 import java.util.ListIterator;
 
+//we can store the ordered collection of objects. It can have duplicate value
+
 public class ArrayListDemo {
-	public static void main(String[]args)
-	{
-		ArrayList al=new ArrayList();
-		System.out.println("initial size of al :"+al.size());
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		System.out.println("initial size of al :" + al.size());
 		al.add("C");
 		al.add("F");
 		al.add("A");
@@ -15,23 +16,21 @@ public class ArrayListDemo {
 		al.add("C");
 		al.add("F");
 		al.add("E");
-				
-		System.out.println("contents of al :"+al);
-		System.out.println("size of al after addition :"+al.size());
-		al.set(2, "X");//replace A with X
-		System.out.println("contents of al after seting A=>X:"+al);
-		System.out.println("size of al after addition :"+al.size());
+
+		System.out.println("contents of al :" + al);
+		System.out.println("size of al after addition :" + al.size());
+		al.set(2, "X");// replace A with X
+		System.out.println("contents of al after seting A=>X:" + al);
+		System.out.println("size of al after addition :" + al.size());
 		al.remove("C");
 		al.remove(2);
-		System.out.println("contents of al after remove :"+al);
-		System.out.println("size of al after addition after remove C and index 2:"+al.size());
-		ListIterator it=al.listIterator();
-		while(it.hasNext())
-		{
+		System.out.println("contents of al after remove :" + al);
+		System.out.println("size of al after addition after remove C and index 2:" + al.size());
+		ListIterator it = al.listIterator();
+		while (it.hasNext()) {
 			System.out.println(it.next());
 		}
-		
-				
+
 	}
 
 }
