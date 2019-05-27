e789a71126662b0ea4f4ce64c8469d2d20c31698
vishwanath Malakar <vishwanathmalakar@gmail.com>
2019-05-27 23:08:23 +0530

    adding new files

--- JS/Display/array.js
+++ JS/Display/array.js
@@ -25,3 +25,21 @@ var myBoys = ["Emil", "Tobias", "Linus"];
 var myChildren = myGirls.concat(myBoys); 
 
 console.log(myChildren);
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.sort(); 
+console.log(fruits);
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.reverse(); 
+console.log(fruits);
+
+//By default, the sort() function sorts values as strings.
+//This works well for strings ("Apple" comes before "Banana").
+//However, if numbers are sorted as strings, "25" is bigger than "100", because "2" is bigger than "1".
+//Because of this, the sort() method will produce incorrect result when sorting numbers.
+//You can fix this by providing a compare function:
+
+var points = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){return a - b});
+console.log(points);
